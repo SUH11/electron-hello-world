de97a2725891bdffa592d4d133763f32c0ac8cf3
huangsuhong <huangsuhong@kingsoft.com>
2020-11-16 19:22:31 +0800

    init

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1200, 
+        height: 700,
+        webPreferences: {
+            webviewTag: true,
+            nativeWindowOpen: true,
+            enableRemoteModule: true
+        }
+    })
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+    mainWindow.webContents.openDevTools()
+
+    console.log('i am ready')
+    setTimeout(() =>{
+        // mainWindow.webContents.print()
+    }, 5000)
+}
+
+app.on('ready', createWindow)
